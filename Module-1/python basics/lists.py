c4461efa328f5c4bb3cc630088e5f1a75ddb2156
anumta04-code <anumtaasifmobile@gmail.com>
2026-08-16 21:16:19 +0500

    Create lists.py

---
+++ Module-1/python basics/lists.py
@@ -0,0 +1,7 @@
+#Python lists
+fruits=["apple","mango","banana","orange"]
+print("my fruits:")
+for fruit in fruits:
+  print(fruit)
+print("first fruit:",fruits[0])
+print(Number of fruits:",len(fruits))
